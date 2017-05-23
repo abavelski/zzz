7f576187a34c29fc91ff3a784fe9cfdbc192d058
Alexei Bavelski <alexei.bavelski@nordea.com>
2017-05-23 14:17:20 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <p>asdfasdf</p>
+      </div>
+    );
+  }
+}
+
+export default App;
